0fff564f78b078c746795f07d4139698d73fc411
MihirKale04 <46636314+MihirKale04@users.noreply.github.com>
2024-05-17 12:04:00 -0700

    Added logging system to simpleServer.py

--- simpleClientServer/simpleServer.py
+++ simpleClientServer/simpleServer.py
@@ -1,4 +1,8 @@
 import socket
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='server.log', level=logging.DEBUG, format='%(message)s %(asctime)s', datefmt='%m/%d/%Y %I:%M:%S %p', filemode = "w")
 
 host = socket.gethostname()
 port = 6969
@@ -10,13 +14,13 @@ server_socket.listen(2)
 
 conn, address = server_socket.accept()
 
-print("Connection from: " + str(address))
+logger.info("Connection from: " + str(address))
 
 while True:
   data = conn.recv(1024).decode()
   if not data:
     break
-  print("from connected user: " + str(data))
+  logger.info("From Client: " + str(data))
   if str(data) == "turn on":
     data = "turning on lights"
   elif str(data) == "turn off":
@@ -24,5 +28,6 @@ while True:
   else:
     data = "Invalid input"
   conn.send(data.encode())
+  logger.info("To Client: " + str(data))
 
 conn.close()
