028f4384758839dbd9f5a238236d3804c1aa580d
MihirKale04 <46636314+MihirKale04@users.noreply.github.com>
2024-05-17 12:03:09 -0700

    Added logging system simpleClient.py

--- simpleClientServer/simpleClient.py
+++ simpleClientServer/simpleClient.py
@@ -1,6 +1,8 @@
 import socket
+import logging
 
-
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='client.log', level=logging.DEBUG, format='%(message)s %(asctime)s', datefmt='%m/%d/%Y %I:%M:%S %p', filemode = "w")
 
 host = socket.gethostname()
 port = 6969
@@ -8,14 +10,16 @@ port = 6969
 client_socket = socket.socket()  # instantiate
 client_socket.connect((host, port))  # connect to the server
 
-message = input(" -> ")
+message = input("To Server: ")
 
 while message.lower().strip() != 'bye':
   client_socket.send(message.encode())  # send message
+  logger.info("Message sent to server: %s", message)
+  
   data = client_socket.recv(1024).decode()  # receive response
+  logger.info("Message recieved from server: %s", data)
+  print('From Server: ' + data)  # show in terminal
 
-  print('Server: ' + data)  # show in terminal
-
-  message = input(" -> ")  # again take input
+  message = input("To Server: ")  # again take input
 
 client_socket.close()  # close the connection
