1e2922162b8920a6aefd375bf1e7e3913cfa50b1
MihirKale04 <46636314+MihirKale04@users.noreply.github.com>
2024-05-16 18:06:26 -0700

    Create simpleClientServer Folder

---
+++ simpleClientServer/simpleServer.py
@@ -0,0 +1,28 @@
+import socket
+
+host = socket.gethostname()
+port = 6969
+
+server_socket = socket.socket()
+server_socket.bind((host, port))
+
+server_socket.listen(2)
+
+conn, address = server_socket.accept()
+
+print("Connection from: " + str(address))
+
+while True:
+  data = conn.recv(1024).decode()
+  if not data:
+    break
+  print("from connected user: " + str(data))
+  if str(data) == "turn on":
+    data = "turning on lights"
+  elif str(data) == "turn off":
+    data = "turning off lights" 
+  else:
+    data = "Invalid input"
+  conn.send(data.encode())
+
+conn.close()
